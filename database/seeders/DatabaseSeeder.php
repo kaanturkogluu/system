1c3dab60c5742d84560d0e799d15f9f76adcd8e3
Kaan Türkoğlu <kaantrrkoglu@gmail.com>
2025-12-20 20:56:26 +0300

    User Seed Hatası Fixlendi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,6 +22,7 @@ class DatabaseSeeder extends Seeder
             ['email' => 'test@example.com'],
             [
                 'name' => 'Test User',
+                'password' => Hash::make('password'),
             ]
         );
     }
